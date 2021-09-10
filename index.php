06302d8b2e27ef71258f89e1702bad8aea068abc
Boris Ts. (cborka) <cborka777@yandex.ru>
2021-09-10 17:07:37 +0700

    2021-09-10

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 
 <head>
     <meta charset="utf-8" />
@@ -13,9 +13,7 @@
     <link rel="shortcut icon" href="favicon.ico" >
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-<!--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>-->
-<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>-->
+
     <?php wp_head() ?>
 
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
@@ -27,18 +25,38 @@
     <div class="grid-container">
         <?php get_header(); ?>
 
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 primary">col-sm-3</div>
+                <div class="col-sm-8 success">col-sm-8</div>
+                <div class="col-sm-4 warning">col-sm-4</div>
+            </div>
+        </div>
+
         <main>
             <section id="intro">
                 <div>Привет, мир!</div>
             </section>
+
+
+
+
         </main>
+
+
+
+
+
 
 <!--        --><?php //get_sidebar(); ?>
     <!--    --><?php //get_sidebar("right"); ?>
         <?php get_footer(); ?>
     </div>
 
-<!-- <script src="js/init.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <!-- <script src="js/init.js"></script> -->
 
 </body>
 
