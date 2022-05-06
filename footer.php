d70894d28f700c1b673831c91a2104a7ba27bce4
Boris Ts. (cborka) <cborka777@yandex.ru>
2022-05-06 16:06:21 +0700

    2022-05-06 добавил счетчики посещений

--- footer.php
+++ footer.php
@@ -13,6 +13,13 @@
 
     <!--    <script src="js/init.js"></script>-->
     <!--    //    alert("aaa");-->
+
+<br>
+
+    <p class='titul' title="Счётчик флагов. На их сайте есть справочник по странам и флагам, только всё на английском">
+        <a href="https://info.flagcounter.com/wcDF">
+            <img src="https://s01.flagcounter.com/count2/wcDF/bg_FFFFFF/txt_000000/border_CCCCCC/columns_8/maxflags_250/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
+    </p>
 </footer>
 </div>
 </body>
--- sidebar.php
+++ sidebar.php
@@ -2,5 +2,10 @@
     <h2>Навигация</h2>
     <?php dynamic_sidebar("sidebar-1"); ?>
 
-    ~~~
+    ~~~<br>
+    <!-- Rating Mail.ru logo -->
+    <a href="https://top.mail.ru/jump?from=3249341">
+        <img src="https://top-fwz1.mail.ru/counter?id=3249341;t=479;l=1" style="border:0;" height="31" width="88" alt="Top.Mail.Ru" /></a>
+    <!-- //Rating Mail.ru logo  -->
+
 </aside>
